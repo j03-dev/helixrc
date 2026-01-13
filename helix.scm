38436ff92d2502617385f59ed73b9df361f5d96d
FITAHIANA Nomeniavo Joe <24nomeniavo@gmail.com>
2026-01-13 09:38:41 +0300

    Update doc for some function

--- helix.scm
+++ helix.scm
@@ -19,33 +19,31 @@
       " ")))
 
 ;;@doc
-;; Adds the current file to git	
+;; Add current file contents to the index
 (define (git-add)
   (shell "git" "add" "%"))
 
-
 ;;@doc
-;; Commit with message 
+;; Record changes to the repository
 (define (git-commit . msg)
   (shell "git" "commit" "-m" (string-join msg " ")))
 
-
+;;@doc
+;; Update remote refs along with associated objects
 (define (git-push)
   (helix.run-shell-command "git push"))
 
-
+;;@doc
+;; Fetch from and integrate with another repository or a local branch
 (define (git-pull)
   (helix.run-shell-command "git pull"))
-
 
 ;;@doc
 ;; Open the helix.scm file
 (define (open-helix-scm)
   (helix.open (helix.static.get-helix-scm-path)))
 
-
 ;;@doc
 ;; Opens the init.scm file
 (define (open-init-scm)
   (helix.open (helix.static.get-init-scm-path)))
-	

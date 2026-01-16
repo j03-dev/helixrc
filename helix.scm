50b7d21ae31f82b6df4b14d8f33b0c93a4804aaf
FITAHIANA Nomeniavo Joe <24nomeniavo@gmail.com>
2026-01-16 17:50:18 +0300

    replace all git function by one function called git

--- helix.scm
+++ helix.scm
@@ -2,48 +2,37 @@
 (require (prefix-in helix. "helix/commands.scm"))
 (require (prefix-in helix.static. "helix/static.scm"))
 
-(provide shell git-add git-push git-pull git-commit open-helix-scm open-init-scm)
+(provide git)
+
+(define (git . args)
+  (let* ([formatted (apply format-args args)]
+         [escaped (map shell-escape formatted)]
+         [cmd (string-append "git " (string-join escaped " "))])
+    (helix.run-shell-command cmd)))
+
+(define (format-args . raw-args)
+  (map (lambda (a)
+        (cond
+          ;; % placeholder
+          [(equal? a "%")
+            (current-path)]
+
+          ;; string → keep as-is
+          [(string? a)
+            a]
+
+          ;; everything else
+          [else
+            (value->string a)]))
+    raw-args))
 
 (define (current-path)
   (let* ([focus (editor-focus)]
          [focus-doc-id (editor->doc-id focus)])
     (editor-document->path focus-doc-id)))
 
-;;@doc
-;; Specialized shell implementation, where % is a wildcard for the current file
-(define (shell . args)
-  (helix.run-shell-command
-    (string-join
-      ;; Replace the % with the current file
-      (map (lambda (x) (if (equal? x "%") (current-path) x)) args)
-      " ")))
-
-;;@doc
-;; Add current file contents to the index
-(define (git-add)
-  (shell "git" "add" "%"))
-
-;;@doc
-;; Record changes to the repository
-(define (git-commit . msg)
-  (shell "git" "commit" "-m" (string-join msg " ")))
-
-;;@doc
-;; Update remote refs along with associated objects
-(define (git-push)
-  (helix.run-shell-command "git push"))
-
-;;@doc
-;; Fetch from and integrate with another repository or a local branch
-(define (git-pull)
-  (helix.run-shell-command "git pull"))
-
-;;@doc
-;; Open the helix.scm file
-(define (open-helix-scm)
-  (helix.open (helix.static.get-helix-scm-path)))
-
-;;@doc
-;; Opens the init.scm file
-(define (open-init-scm)
-  (helix.open (helix.static.get-init-scm-path)))
+(define (shell-escape s)
+  (if (or (string-contains? s " ")
+       (string-contains? s "'"))
+    (string-append "'" s "'")
+    s))

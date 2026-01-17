f745430e287548f3d3eaa3c73f6e6e9c036341c1
FITAHIANA Nomeniavo Joe <24nomeniavo@gmail.com>
2026-01-17 19:16:08 +0300

    fix confict
    Signed-off-by: FITAHIANA Nomeniavo Joe <24nomeniavo@gmail.com>

--- helix.scm
+++ helix.scm
@@ -1,22 +1,14 @@
 (require "helix/editor.scm")
-(require (prefix-in helix. "helix/commands.scm"))
-<<<<<<< HEAD
-=======
-(require (prefix-in helix.static. "helix/static.scm"))
->>>>>>> parent of 4a03ba8... format the code and add formater for schema
+(require (only-in "helix/commands.scm" run-shell-command))
 
-(provide git)
+(provide git current-path)
 
 (define (git . args)
   (let* ([formatted (apply format-args args)]
          [escaped (map shell-escape formatted)]
          [cmd (string-append "git " (string-join escaped " "))])
-<<<<<<< HEAD
-    (helix.run-shell-command cmd))
-  (helix.reload (current-path)))
-=======
-    (helix.run-shell-command cmd)))
->>>>>>> parent of 4a03ba8... format the code and add formater for schema
+    (run-shell-command cmd))
+  (editor-document-reload))
 
 (define (format-args . raw-args)
   (map (lambda (a)

ee1c6eb5262d8de005a388057c74aac976a32203
FITAHIANA Nomeniavo Joe <24nomeniavo@gmail.com>
2026-01-17 19:15:02 +0300

    Revert "format the code and add formater for schema"
    This reverts commit 4a03ba818f4f12294eef4ea077d3641fac156de2.

--- helix.scm
+++ helix.scm
@@ -1,14 +1,22 @@
 (require "helix/editor.scm")
 (require (prefix-in helix. "helix/commands.scm"))
+<<<<<<< HEAD
+=======
+(require (prefix-in helix.static. "helix/static.scm"))
+>>>>>>> parent of 4a03ba8... format the code and add formater for schema
 
-(provide git current-path)
+(provide git)
 
 (define (git . args)
   (let* ([formatted (apply format-args args)]
          [escaped (map shell-escape formatted)]
          [cmd (string-append "git " (string-join escaped " "))])
+<<<<<<< HEAD
     (helix.run-shell-command cmd))
   (helix.reload (current-path)))
+=======
+    (helix.run-shell-command cmd)))
+>>>>>>> parent of 4a03ba8... format the code and add formater for schema
 
 (define (format-args . raw-args)
   (map (lambda (a)

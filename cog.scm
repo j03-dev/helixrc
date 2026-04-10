d36f893e8cfd0a1636cf5d0f200fab575d7d73eb
FITAHIANA Nomeniavo Joe <24nomeniavo@gmail.com>
2026-04-10 08:38:37 +0300

    chore: add all package needed for my config

---
+++ cog.scm
@@ -0,0 +1,8 @@
+(define package-name 'j03-dev-hx)
+(define version "0.1.0")
+
+(define dependencies
+  '((#:name system-theme-hx #:git-url "https://github.com/j03-dev/system-theme.hx.git")
+    (#:name file-tree-hx #:git-url "https://github.com/j03-dev/file-tree.hx.git")))
+
+(define dylibs '())
--- init.scm
+++ init.scm
@@ -1,3 +1,7 @@
 (require "system-theme-hx/system-theme.scm")
-
 (auto-theme "onedarker" "gruvbox_light_hard")
+
+(require "helix/keymaps.scm")
+(require "file-tree-hx/file-tree.scm")
+
+(keymap (buffer FILE-TREE) (with-map FILE-TREE-KEYBINDINGS))

543e62c7dee7f19fa849199642be52eb7b090838
FITAHIANA Nomeniavo Joe <24nomeniavo@gmail.com>
2026-06-03 19:06:11 +0300

    chore: go back to pylsp

--- init.scm
+++ init.scm
@@ -1,5 +1,5 @@
 (require "system-theme-hx/system-theme.scm")
-(auto-theme "onedarker" "gruvbox_light_hard")
+(auto-theme "monokai_aqua" "gruvbox_light_hard")
 
 (require "helix/keymaps.scm")
 (require "file-tree-hx/file-tree.scm")
